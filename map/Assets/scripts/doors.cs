682efadb2df50b57b4c64384917e7c2d3bcf00f8
WeenGood <provotorov24@gmail.com>
2018-08-26 21:20:56 +0300

    Add doors and other little changes

---
+++ map/Assets/scripts/doors.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doors : MonoBehaviour
+{
+
+    public bool open;
+    public Animation anim;
+    bool iN;
+    public AnimationClip[] anima;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = this.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && iN)
+        {
+            open = !open;
+            if (iN)
+                if (open)
+                {
+                    anim.CrossFade(anima[0].name);
+                }
+                else
+                    anim.CrossFade(anima[1].name);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            iN = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            iN = false;
+        }
+    }
+}
